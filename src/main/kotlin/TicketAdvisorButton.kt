3f7aaac193ce0cbfc0cf7d1ac66a7d8d4b2c68ad
megamegax <mhunyady@emarsys.com>
2018-08-29 09:39:02 +0200

    feat(ide) add plugin.xml

---
+++ src/main/kotlin/TicketAdvisorButton.kt
@@ -0,0 +1,9 @@
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class TicketAdvisorButton : AnAction() {
+
+    override fun actionPerformed(e: AnActionEvent?) {
+        println("test")
+    }
+}
