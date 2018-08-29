1befdd2db00c7bceaa5f8f61989f08f85d8fb34c
megamegax <mhunyady@emarsys.com>
2018-08-29 18:11:09 +0200

    feat(koin) move koin initialization to entry point of the plugin

--- src/main/kotlin/TicketAdvisorButton.kt
+++ src/main/kotlin/TicketAdvisorButton.kt
@@ -1,7 +1,21 @@
+import client.Client
+import client.ClientService
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import org.koin.dsl.module.applicationContext
+import org.koin.standalone.StandAloneContext
 
 class TicketAdvisorButton : AnAction() {
+    val baseUrl = "http://index.hu"
+    // Koin module
+    val clientModule = applicationContext {
+        bean { ClientService(baseUrl) as Client }
+    }
+
+    init {
+        StandAloneContext.startKoin(listOf(clientModule))
+
+    }
 
     override fun actionPerformed(e: AnActionEvent?) {
         println("test")
